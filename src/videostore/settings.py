6479da6773ad0e4d858280728f2116ebfbb4717f
ZeroCoolHacker <shahzaibakash@gmail.com>
2019-01-17 01:23:33 +0500

    Add Membership Models

--- src/videostore/settings.py
+++ src/videostore/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'memberships',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_root/'),
+]
+
+VENV_PATH = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(VENV_PATH, 'media_root')
+
+
+if DEBUG:
+    STRIPE_PUBLISHABLE_KEY = 'pk_test_sV4OhR76FLQQqhtzqQAWZ3cd'
+    STRIPE_SECRET_KEY = 'sk_test_3CVmJGwlNAdQ2OA3BWTHg0A7'
+else:
+    STRIPE_PUBLISHABLE_KEY = ''
+    STRIPE_SECRET_KEY = ''
+    
