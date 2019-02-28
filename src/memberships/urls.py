9d667cf77c6dc11773a7f68c4499fa56a2d27778
ZeroCoolHacker <shahzaibakash@gmail.com>
2019-03-01 01:19:30 +0500

    add payment url

--- src/memberships/urls.py
+++ src/memberships/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
 
-from .views import MembershipSelectView
+from .views import MembershipSelectView, payment_view
 
 app_name = 'memberships'
 
 urlpatterns = [
     path('', MembershipSelectView.as_view(), name='select'),
+    path('payment', payment_view, name='payment')
 ]
